8a159978b82381cc07ffbb14f76254e6ba2efa49
Nicolas Fontes <nicolas.fontes@planetpayment.com>
2021-09-14 13:23:28 +0100

    Enable Swagger Documentation Page on Production environment

--- MinMaNet.API/Startup.cs
+++ MinMaNet.API/Startup.cs
@@ -40,9 +40,10 @@ namespace MinMaNet.API
 			if (env.IsDevelopment())
 			{
 				app.UseDeveloperExceptionPage();
-				app.UseSwagger();
-				app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "MinMaNet.API v1"));
 			}
+
+			app.UseSwagger();
+			app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "MinMaNet.API v1"));
 
 			app.UseHttpsRedirection();
 
